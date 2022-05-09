01df1de542d77c2ab27c77ff94a0401c6d60126c
Eugene Katkov <eukatkov@gmail.com>
2022-05-09 17:55:56 +0400

    Добавлены свойства allocated_quantity и available_quantity, исправлен метод allocate в классе Coil
    allocated_quantity - суммарное размещенное количество материала;
    available_quantity - доступное количество материала
    Метод allocate исправлен в соответствии с добавленными свойствами

--- allocation/domain/domain_logic.py
+++ allocation/domain/domain_logic.py
@@ -12,12 +12,22 @@ class Coil:
     def __init__(self, reference: str, product_id: str, quantity: int):
         self.reference = reference
         self.product_id = product_id
-        self.available_quantity = quantity
+        self._initial_quantity = quantity
+        self._allocations: set[OrderLine] = set()
 
-    def allocate(self, line: OrderLine):
-        self.available_quantity -= line.quantity
+    @property
+    def allocated_quantity(self):
+        return sum(line.quantity for line in self._allocations)
 
-    def can_allocate(self, line: OrderLine):
+    @property
+    def available_quantity(self):
+        return self._initial_quantity - self.allocated_quantity
+
+    def can_allocate(self, line: OrderLine) -> bool:
         return (self.product_id == line.product_id) and (
             self.available_quantity >= line.quantity)
+
+    def allocate(self, line: OrderLine):
+        if self.can_allocate(line):
+            self._allocations.add(line)
 
