1f29a165045d4bb3a12dbdebf26e9c565b84abc9
Eugene Katkov <eukatkov@gmail.com>
2022-05-10 01:46:47 +0400

    Добавлена функция allocate_to_list_of_coils и необходимые для ее работы функции в классе Coil, а также тесты
    Функция allocate_to_list_of_coils размещает товарную позицию для подходящей бухты из списка. После размещения оставшееся количество материала в бухте должно быть >= рекомендуемого остатка или <= приемлемой потери материала

--- allocation/domain/domain_logic.py
+++ allocation/domain/domain_logic.py
@@ -9,10 +9,13 @@ class OrderLine:
 
 
 class Coil:
-    def __init__(self, reference: str, product_id: str, quantity: int):
+    def __init__(self, reference: str, product_id: str,
+                 quantity: int, recommended_balance: int, acceptable_loss: int):
         self.reference = reference
         self.product_id = product_id
         self._initial_quantity = quantity
+        self.recommended_balance = recommended_balance
+        self.acceptable_loss = acceptable_loss
         self._allocations: set[OrderLine] = set()
 
     @property
@@ -34,3 +37,26 @@ class Coil:
     def deallocate(self, line: OrderLine):
         if line in self._allocations:
             self._allocations.remove(line)
+
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, Coil):
+            return False
+        return self.reference == other.reference
+
+    def __lt__(self, other) -> bool:
+        if not isinstance(other, Coil):
+            return False
+        return self.available_quantity < other.available_quantity
+
+    def __hash__(self):
+        return hash(self.reference)
+
+
+def allocate_to_list_of_coils(line: OrderLine, coils: list[Coil]) -> str:
+    coil = next(c for c in sorted(coils) if (
+        c.can_allocate(line) and
+        ((c.available_quantity - line.quantity) >= c.recommended_balance or
+         (c.available_quantity - line.quantity) <= c.acceptable_loss)
+    ))
+    coil.allocate(line)
+    return coil.reference
---
+++ tests/unit/test_allocate.py
@@ -0,0 +1,59 @@
+from allocation.domain.domain_logic import Coil, OrderLine, allocate_to_list_of_coils
+
+
+def test_prefers_smaller_coil():
+    smaller_coil = Coil('Бухта-005', 'АВВГ_2х6', 95, 5, 1)
+    medium_coil = Coil('Бухта-005', 'АВВГ_2х6', 96, 5, 1)
+    bigger_coil = Coil('Бухта-005', 'АВВГ_2х6', 97, 5, 1)
+    line = OrderLine('Заказ-010', 'АВВГ_2х6', 15)
+
+    allocate_to_list_of_coils(
+        line, [bigger_coil, smaller_coil, medium_coil]
+    )
+
+    assert smaller_coil.available_quantity == 80
+    assert medium_coil.available_quantity == 96
+    assert bigger_coil.available_quantity == 97
+
+
+def test_prefers_coil_with_suitable_balance():
+    right_coil = Coil('Бухта-006', 'АВВГ_2х6', 20, 4, 1)
+    wrong_coil_1 = Coil('Бухта-006', 'АВВГ_2х6', 20, 6, 1)
+    wrong_coil_2 = Coil('Бухта-006', 'АВВГ_2х6', 20, 8, 1)
+    line = OrderLine('Заказ-011', 'АВВГ_2х6', 15)
+
+    allocate_to_list_of_coils(
+        line, [wrong_coil_1, right_coil, wrong_coil_2]
+    )
+
+    assert right_coil.available_quantity == 5
+    assert wrong_coil_1.available_quantity == 20
+    assert wrong_coil_2.available_quantity == 20
+
+
+def test_prefers_coil_with_suitable_loss():
+    right_coil = Coil('Бухта-007', 'АВВГ_2х6', 20, 8, 4)
+    wrong_coil_1 = Coil('Бухта-007', 'АВВГ_2х6', 20, 8, 2)
+    wrong_coil_2 = Coil('Бухта-007', 'АВВГ_2х6', 20, 8, 1)
+    line = OrderLine('Заказ-011', 'АВВГ_2х6', 17)
+
+    allocate_to_list_of_coils(
+        line, [wrong_coil_1, right_coil, wrong_coil_2]
+    )
+
+    assert right_coil.available_quantity == 3
+    assert wrong_coil_1.available_quantity == 20
+    assert wrong_coil_2.available_quantity == 20
+
+
+def test_returns_allocated_coil_reference():
+    smaller_coil = Coil('Бухта-008', 'АВВГ_2х6', 95, 10, 3)
+    medium_coil = Coil('Бухта-008', 'АВВГ_2х6', 105, 7, 2)
+    bigger_coil = Coil('Бухта-008', 'АВВГ_2х6', 120, 3, 1)
+    line = OrderLine('Заказ-012', 'АВВГ_2х6', 30)
+
+    allocation = allocate_to_list_of_coils(
+        line, [bigger_coil, smaller_coil, medium_coil]
+    )
+
+    assert smaller_coil.reference == allocation
