7c18b94a49b2b85d70944a00b6849aff4f63f3c9
Eugene Katkov <eukatkov@gmail.com>
2022-06-12 11:40:11 +0400

    Исправления опечаток и добавление комментариев в тестах модуля test_api_coils.py

--- tests/end_to_end/test_api_coils.py
+++ tests/end_to_end/test_api_coils.py
@@ -9,34 +9,34 @@ from allocation.services import unit_of_work
 
 @pytest.mark.django_db(transaction=True)
 def test_add_a_coil():
-    data = {"reference": 'Бухта-0010', "product_id": "АВВГ_2х2,5",
-            "quantity": 150, "recommended_balance": 10, "acceptable_loss": 2}
+    coil_data = {"reference": 'Бухта-0010', "product_id": "АВВГ_2х2,5",
+                 "quantity": 150, "recommended_balance": 10, "acceptable_loss": 2}
     client = APIClient()
-    output_data = json.dumps(data, ensure_ascii=False)
+    input_coil_data = json.dumps(coil_data, ensure_ascii=False)
 
-    response = client.post('/v1/coils', data=output_data, format='json')
-    input_data = json.loads(response.data)
+    response = client.post('/v1/coils', data=input_coil_data, format='json')
+    output_data = json.loads(response.data)
 
-    assert input_data['message'] == 'OK'
+    assert output_data['message'] == 'OK'
     assert response.status_code == 200
 
 
 @pytest.mark.django_db(transaction=True)
 def test_add_a_coil_is_idempotent():
-    data_1 = {"reference": 'Бухта-015', "product_id": "АВВГ_2х2,5",
-              "quantity": 150, "recommended_balance": 10, "acceptable_loss": 2}
-    data_2 = {"reference": 'Бухта-015', "product_id": "АВВГ_3х1,5",
-              "quantity": 200, "recommended_balance": 12, "acceptable_loss": 1}
+    coil_data_1 = {"reference": 'Бухта-015', "product_id": "АВВГ_2х2,5",
+                   "quantity": 150, "recommended_balance": 10, "acceptable_loss": 2}
+    coil_data_2 = {"reference": 'Бухта-015', "product_id": "АВВГ_3х1,5",
+                   "quantity": 200, "recommended_balance": 12, "acceptable_loss": 1}
     client = APIClient()
-    output_data_1 = json.dumps(data_1, ensure_ascii=False)
-    output_data_2 = json.dumps(data_2, ensure_ascii=False)
+    input_coil_data_1 = json.dumps(coil_data_1, ensure_ascii=False)
+    input_coil_data_2 = json.dumps(coil_data_2, ensure_ascii=False)
 
-    client.post('/v1/coils', data=output_data_1, format='json')
-    response = client.post('/v1/coils', data=output_data_2, format='json')
-    input_data = json.loads(response.data)
+    client.post('/v1/coils', data=input_coil_data_1, format='json')
+    response = client.post('/v1/coils', data=input_coil_data_2, format='json')
+    output_data = json.loads(response.data)
 
-    assert input_data['message'] == \
-           f'Запись с reference={data_2["reference"]} уже существует в таблице Coil базы данных'
+    assert output_data['message'] == \
+           f'Запись с reference={coil_data_2["reference"]} уже существует в таблице Coil базы данных'
     assert response.status_code == 400
 
 
@@ -44,15 +44,15 @@ def test_add_a_coil_is_idempotent():
 def test_add_a_coil_raise_validation_error():
     # reference имеет неверное имя, quantity и recommended_balance имеют отрицательные значения.
     # Итого три несоответствия CoilBaseModel
-    data = {"reference": 'Бухт-020', "product_id": "АВВГ_2х2,5",
-            "quantity": -70, "recommended_balance": -10, "acceptable_loss": 2}
+    coil_data = {"reference": 'Бухт-020', "product_id": "АВВГ_2х2,5",
+                 "quantity": -70, "recommended_balance": -10, "acceptable_loss": 2}
     client = APIClient()
-    output_data = json.dumps(data, ensure_ascii=False)
+    input_data = json.dumps(coil_data, ensure_ascii=False)
 
-    response = client.post('/v1/coils', data=output_data, format='json')
-    input_data = json.loads(response.data)
+    response = client.post('/v1/coils', data=input_data, format='json')
+    output_data = json.loads(response.data)
 
-    assert '3 validation errors for CoilBaseModel' in input_data['message']
+    assert '3 validation errors for CoilBaseModel' in output_data['message']
     assert response.status_code == 400
 
 
@@ -61,36 +61,40 @@ def test_get_a_coil():
     client = APIClient()
     # Добавление coil_data в базу данных с помощью post запроса
     coil_data = {"reference": 'Бухта-021', "product_id": "АВВГ_2х2,5",
-                 "quantity": 200, "recommended_balance": 12, "acceptable_loss": 3}
-    output_coil_data = json.dumps(coil_data, ensure_ascii=False)
-    client.post('/v1/coils', data=output_coil_data, format='json')
-    # Добавление line_data в базу данных с помощью post запроса
+                 "quantity": 220, "recommended_balance": 12, "acceptable_loss": 3}
+    input_coil_data = json.dumps(coil_data, ensure_ascii=False)
+    client.post('/v1/coils', data=input_coil_data, format='json')
+    # Добавление orderline в базу данных и дальнейшее размещение (allocation) с помощью post запросов
     line_data = {"order_id": 'Заказ-017', "line_item": "Позиция-001",
-                 "product_id": 'АВВГ_3х2,5', "quantity": 40}
-    output_line_data = json.dumps(line_data, ensure_ascii=False)
-    client.post('/v1/orderlines', data=output_line_data, format='json')
+                 "product_id": 'АВВГ_2х2,5', "quantity": 40}
+    input_line_data = json.dumps(line_data, ensure_ascii=False)
+    client.post('/v1/orderlines', data=input_line_data, format='json')
+    client.post('/v1/allocate', data=input_line_data, format='json')
 
     response = client.get(f"/v1/coils/{coil_data['reference']}")
-    input_data = json.loads(response.data)
+    output_coil = json.loads(response.data)
+    # множество из значений order_id размещенных (allocated) orderlines для полученного output_coil
+    output_allocated_lines_order_ids = {json.loads(line)['order_id'] for line in output_coil['allocations']}
 
-    assert input_data['reference'] == 'Бухта-021'
-    assert input_data['quantity'] == 200
+    assert output_coil['reference'] == 'Бухта-021'
+    assert output_coil['quantity'] == 220
+    assert output_allocated_lines_order_ids == {line_data['order_id']}
     assert response.status_code == 200
 
 
 @pytest.mark.django_db(transaction=True)
 def test_get_a_coil_raise_not_exist_error():
-    data = {"reference": 'Бухта-023', "product_id": "АВВГ_2х2,5",
-            "quantity": 170, "recommended_balance": 12, "acceptable_loss": 3}
+    coil_data = {"reference": 'Бухта-023', "product_id": "АВВГ_2х2,5",
+                 "quantity": 170, "recommended_balance": 12, "acceptable_loss": 3}
     client = APIClient()
-    output_data = json.dumps(data, ensure_ascii=False)
-    client.post('/v1/coils', data=output_data, format='json')
+    input_coil_data = json.dumps(coil_data, ensure_ascii=False)
+    client.post('/v1/coils', data=input_coil_data, format='json')
     wrong_reference = 'Бухта-005'
 
     response = client.get(f"/v1/coils/{wrong_reference}")
-    input_data = json.loads(response.data)
+    output_data = json.loads(response.data)
 
-    assert input_data['message'] == \
+    assert output_data['message'] == \
            f"Запись с reference={wrong_reference} отсутствует в таблице Coil базы данных"
     assert response.status_code == 400
 
@@ -99,18 +103,18 @@ def test_get_a_coil_raise_not_exist_error():
 def test_get_a_coil_raise_validation_error():
     # запись добавляется в базу данных с помощью UnitOfWork
     # запись имеет ошибки в quantity и recommended_balance
-    data = {'reference': 'Бухта-025', 'product_id': 'АВВГ_2х2,5',
-            'quantity': -150, 'recommended_balance': -10, 'acceptable_loss': 2}
+    coil_data = {'reference': 'Бухта-025', 'product_id': 'АВВГ_2х2,5',
+                 'quantity': -150, 'recommended_balance': -10, 'acceptable_loss': 2}
     uow = unit_of_work.DjangoCoilUnitOfWork()
     with uow:
-        coil = domain_logic.Coil(data['reference'], data['product_id'], data['quantity'],
-                                 data['recommended_balance'], data['acceptable_loss'])
+        coil = domain_logic.Coil(coil_data['reference'], coil_data['product_id'], coil_data['quantity'],
+                                 coil_data['recommended_balance'], coil_data['acceptable_loss'])
         uow.coil_repo.add(coil)
         uow.commit()
     client = APIClient()
 
-    response = client.get(f"/v1/coils/{data['reference']}")
-    input_data = json.loads(response.data)
+    response = client.get(f"/v1/coils/{coil_data['reference']}")
+    output_data = json.loads(response.data)
 
-    assert '2 validation errors for CoilBaseModel' in input_data['message']
+    assert '2 validation errors for CoilBaseModel' in output_data['message']
     assert response.status_code == 500
