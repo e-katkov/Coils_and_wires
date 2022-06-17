34c17ebd61bbbd5acaea1f30a52ff563bb02eaa3
Eugene Katkov <eukatkov@gmail.com>
2022-06-17 11:57:14 +0400

    Создан метод update_a_line() модуля services.py

--- allocation/services/services.py
+++ allocation/services/services.py
@@ -67,6 +67,51 @@ def add_a_line(
         uow.commit()
 
 
+def update_a_line(
+        order_id: str,
+        line_item: str,
+        product_id: str,
+        quantity: int,
+        uow_line: unit_of_work.AbstractOrderLineUnitOfWork,
+        uow_coil: unit_of_work.AbstractCoilUnitOfWork
+) -> domain_logic.Coil:
+    # db_line - это orderline, которую необходимо обновить
+    # Определение allocation_coil, куда размещена db_line
+    with uow_line:
+        db_line = uow_line.line_repo.get(order_id=order_id, line_item=line_item)
+        allocation_coil = uow_line.line_repo.get_an_allocation_coil(db_line)
+    # Удаление db_line из множества allocations найденного allocation_coil
+    with uow_coil:
+        if not allocation_coil.reference == 'fake':
+            allocation_coil.deallocate(db_line)
+            uow_coil.coil_repo.update(allocation_coil)
+            uow_coil.commit()
+    # Обновление db_line до input_line
+    with uow_line:
+        input_line = domain_logic.OrderLine(order_id, line_item, product_id, quantity)
+        uow_line.line_repo.update(input_line)
+        uow_line.commit()
+    # Размещение (allocation) input_line
+    # В первую очередь выполняется попытка разместить input_line в найденный allocation_coil
+    # Возврат "поддельного" coil в случае, если изначально allocation_coil отсутствовал
+    with uow_coil:
+        if allocation_coil.reference == 'fake':
+            fake_coil = allocation_coil
+            return fake_coil
+        else:
+            if allocation_coil.can_allocate(input_line):
+                allocation_coil.allocate(input_line)
+                uow_coil.coil_repo.update(allocation_coil)
+                uow_coil.commit()
+                return allocation_coil
+            else:
+                list_of_coils = uow_coil.coil_repo.list()
+                allocation_coil = domain_logic.allocate_to_list_of_coils(line=input_line, coils=list_of_coils)
+                uow_coil.coil_repo.update(allocation_coil)
+                uow_coil.commit()
+                return allocation_coil
+
+
 def allocate(
         order_id: str,
         line_item: str,
