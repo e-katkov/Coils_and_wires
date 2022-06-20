74393210463974474d3900cf9e65d28bbbfa5728
Eugene Katkov <eukatkov@gmail.com>
2022-06-20 21:23:36 +0400

    Создан urlpattern в модуле api/urls.py для обработки GET запросов на 'allocate/<str:order_id>/<str:line_item>'

--- allocation/api/urls.py
+++ allocation/api/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('coils/<str:reference>', CoilDetail.as_view()),
     path('orderlines/<str:order_id>/<str:line_item>', OrderLineDetail.as_view()),
     path('allocate', Allocate.as_view()),
+    path('allocate/<str:order_id>/<str:line_item>', Allocate.as_view()),
 ]
