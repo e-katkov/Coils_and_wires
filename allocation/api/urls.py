bd15c3e91fe2a9f3163f681244c21e0e8b599ba7
Eugene Katkov <eukatkov@gmail.com>
2022-07-02 11:58:15 +0400

    Исправлены urlpatterns модуля allocation/api/urls.py
    Необходимо для корректной работы библиотеки drf-spectacular

--- allocation/api/urls.py
+++ allocation/api/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from allocation.api.api_views import Coil, OrderLine, Allocate
+from allocation.api import api_views
 
 urlpatterns = [
-    path('coils', Coil.as_view()),
-    path('coils/<str:reference>', Coil.as_view()),
-    path('orderlines', OrderLine.as_view()),
-    path('orderlines/<str:order_id>/<str:line_item>', OrderLine.as_view()),
-    path('allocate', Allocate.as_view()),
-    path('allocate/<str:order_id>/<str:line_item>', Allocate.as_view()),
+    path('coils', api_views.Coil.as_view()),
+    path('coils/<str:reference>', api_views.CoilDetail.as_view()),
+    path('orderlines', api_views.OrderLine.as_view()),
+    path('orderlines/<str:order_id>/<str:line_item>', api_views.OrderLineDetail.as_view()),
+    path('allocate', api_views.Allocate.as_view()),
+    path('allocate/<str:order_id>/<str:line_item>', api_views.AllocateDetail.as_view()),
 ]
