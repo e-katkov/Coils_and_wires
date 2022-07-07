00e7f2758306c465c5527e0cf6098121a2c628ef
Eugene Katkov <eukatkov@gmail.com>
2022-07-07 18:42:27 +0400

    Исправлено оформление - удалены лишние переносы строк (flake8, E127)

--- tests/unit/test_reallocate.py
+++ tests/unit/test_reallocate.py
@@ -7,9 +7,7 @@ def test_can_reallocate_enough_orderlines(dict_of_orderlines):
     размещенных товарных позиций после их переразмещения.
     """
     coil = Coil('Бухта-001', 'АВВГ_3х1,5', 60, 15, 3)
-    coil.allocations = dict_of_orderlines['set_0'] | \
-                       dict_of_orderlines['set_1'] | \
-                       dict_of_orderlines['set_2']
+    coil.allocations = dict_of_orderlines['set_0'] |dict_of_orderlines['set_1'] | dict_of_orderlines['set_2']
     coil_allocations_quantities = {line.quantity for line in coil.allocations}
 
     new_coil = Coil('Бухта-001', 'АВВГ_3х1,5', 80, 15, 3)
@@ -25,9 +23,7 @@ def test_cannot_reallocate_one_orderline(dict_of_orderlines):
     размещенных товарных позиций после их переразмещения.
     """
     coil = Coil('Бухта-001', 'АВВГ_3х1,5', 60, 15, 3)
-    coil.allocations = dict_of_orderlines['set_0'] | \
-                       dict_of_orderlines['set_1'] | \
-                       dict_of_orderlines['set_2']
+    coil.allocations = dict_of_orderlines['set_0'] | dict_of_orderlines['set_1'] | dict_of_orderlines['set_2']
     quantities_of_set_0_set_1 = {line.quantity for line in (dict_of_orderlines['set_0'] |
                                                             dict_of_orderlines['set_1'])}
 
@@ -44,9 +40,7 @@ def test_cannot_reallocate_two_orderlines(dict_of_orderlines):
     в бухте изменит состав размещенных товарных позиций после их переразмещения.
     """
     coil = Coil('Бухта-001', 'АВВГ_3х1,5', 60, 15, 3)
-    coil.allocations = dict_of_orderlines['set_0'] |\
-                       dict_of_orderlines['set_1'] |\
-                       dict_of_orderlines['set_2']
+    coil.allocations = dict_of_orderlines['set_0'] | dict_of_orderlines['set_1'] | dict_of_orderlines['set_2']
     quantities_of_set_0 = {line.quantity for line in dict_of_orderlines['set_0']}
 
     new_coil = Coil('Бухта-001', 'АВВГ_3х1,5', 60, 17, 2)
